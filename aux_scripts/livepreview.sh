b15ffe2195c92b9f0adce7e574611b5a7332dc02
Kos Ivantsov <kos.ivantsov@gmail.com>
2022-01-11 17:36:38 +0200

    Create livepreview.sh

---
+++ aux_scripts/livepreview.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+PDFVIEWER=zathura
+CURRENTDOC=$1
+FILENAME=$2
+PROJROOT=$3
+PDFDIR="${PROJROOT}pdf"
+if [ ! -d "$PDFDIR" ]; then
+    mkdir "$PDFDIR"
+fi
+ln -sf $CURRENTDOC $PDFDIR
+cd $PDFDIR
+mv $(basename ${FILENAME}) current.${FILENAME##*.}  
+lowriter --convert-to pdf --outdir $PDFDIR $PDFDIR/current.${FILENAME##*.} > /dev/null 2>&1
+# ps ux|grep zathura|grep current.pdf > /tmp/omt.txt
+if [ -z "$(ps ux|grep $PDFVIEWER|grep current.pdf)" ]; then
+    $PDFVIEWER $PDFDIR/current.pdf
+fi
+echo "Previewing current target file"

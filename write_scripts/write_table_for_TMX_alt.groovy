5d9e4a04759deb16063ce878d95f1c71d0041206
Kos Ivantsov <kos.ivantsov@gmail.com>
2022-01-31 14:58:57 +0200

    Update write_table_for_TMX_alt.groovy
    CLI-runnable

--- write_scripts/write_table_for_TMX_alt.groovy
+++ write_scripts/write_table_for_TMX_alt.groovy
@@ -11,8 +11,8 @@
  *            +--------------+--------------+--------------+
  * 
  * @author:   Kos Ivantsov
- * @date:     2019-02-26
- * @version:  0.3
+ * @date:     2022-01-31
+ * @version:  0.4
  */
 import java.awt.Desktop
 import org.omegat.core.Core
@@ -24,28 +24,29 @@ import static javax.swing.JOptionPane.*
 import static org.omegat.util.Platform.*
 
 //// Script Options
-autoopen    = "none"  //Automatically open the table file upon creation ("folder"|"table"|"none")
+all_project = true      //Set to true to export all project, only the current file will be exported
+autoopen    = "none"    //Automatically open the table file upon creation ("folder"|"table"|"none")
 skipUntran  = true      //Skip untranslated segments (true|false)
-fillEmptTran= false     //Add custom string to empty translations (true|false)
-EmptTranTxt = "<EMPTY>" //String to be replace empty translation (in quotes, ignored if above is false)
-markNonUniq = false     //Add color background to non-unique segments
+fillEmptTran= false     //Add custom string to empty translations, i.e. where translation is INTENTIONALLY set to empty (true|false)
+EmptTranTxt = "<EMPTY>" //String to replace empty translations (in quotes, ignored if above is false)
+markNonUniq = true      //Add color background to non-unique segments
 markAlt     = true      //Add color frame around segments with alternative translation
 addExtraCol = true      //Add column with info about uniqness and alternative translation
-extraColStr = "XX"      //Uniq column caption
+extraColStr = "XX"      //Uniq column caption (in quotes, ignored if above is false)
 uniqStr     = ""        //Cell mark for uniq segments
-firstStr    = "1"       //Cell mark for 1st occurance of repeated segment
-repStr      = "+"       //Cell mark for further occurances of repeated segment
+firstStr    = "1"       //Cell mark for the 1st occurance of a repeated segment
+repStr      = "+"       //Cell mark for further occurances of a repeated segment
 altStr      = "a"       //Cell mark fo alternative translation of the segmnent
 defStr      = ""        //Cell mark fo default translation of the segmnent
 
 //// Colors
-normfg    = '#000000'
-curtagbg  = '#BCC6DD'        //tag background in segment's text
-curtagfg  = 'green'          //tag foreground in segment's text
-curtxtbg  = '#BCC6CC'        //font background in segment's text
-nonuniqfg = '#595959'
-nonuniqbg = '#F4F6F7'
-alttxtbg  = '#C73A3A'
+normfg    = '#000000'   //foreground
+curtagbg  = '#BCC6DD'   //tag background in segment's text
+curtagfg  = 'green'     //tag foreground in segment's text
+curtxtbg  = '#BCC6CC'   //font background in segment's text
+nonuniqfg = '#595959'   //foreground for non-uniq segments
+nonuniqbg = '#F4F6F7'   //background for non-uniq segments
+alttxtbg  = '#C73A3A'   //background for alternative translations
 
 //// UI Strings
 name = "Export project to table"
@@ -54,6 +55,23 @@ countWritten = "{0} segments written to {1}."
 tableWritten = "Table {0} created. {1} segments written."
 noTable = "Nothing to export. {0} wasn't created."
 
+//// CLI or GUI probing
+def echo
+def cli
+try {
+    console.println("")
+    echo = {
+        k -> console.println(k.toString())
+    }
+    cli = false
+} catch(Exception e) {
+    echo = { k -> 
+        println("\n~~~ Script output ~~~\n\n" + k.toString() + "\n\n^^^^^^^^^^^^^^^^^^^^^\n")
+    }
+    cli = true
+}
+
+//// Try to use external resources, or switch to the hardcoded strings
 resBundle = { k,v ->
     try {
         v = res.getString(k)
@@ -63,6 +81,7 @@ resBundle = { k,v ->
     }
 }
 
+//// Here goes nothing, let's have some fun!
 String.metaClass.confirm = { ->
     showConfirmDialog null, delegate, title, YES_NO_OPTION
 }
@@ -72,8 +91,9 @@ String.metaClass.alert = { ->
 }
 
 utils = (StringUtil.getMethods().toString().findAll("makeValidXML")) ? StringUtil : StaticUtils
-
-console.clear()
+if (!cli) {
+    console.clear()
+}
 prop = project.projectProperties
 if (!prop){
     title = resBundle("name", name)
@@ -119,91 +139,95 @@ table_contents << """\
       $targHead
     </tr>\n
 """
-files = project.projectFiles.subList(editor.@displayedFileIndex, editor.@displayedFileIndex + 1); 
-    for (i in 0 ..< files.size())
-    {
-        fi = files[i]
-        for (j in 0 ..< fi.entries.size()){
-            def cellbg
-            def altbg
-            def border = "border:1px solid transparent"
-            def ignore = false
-            ste = fi.entries[j]
-            info = project.getTranslationInfo(ste)
-            def isDup = ste.getDuplicate()
-            def isAlt = info.defaultTranslation ? defStr : altStr
-            def newPar = ste.paragraphStart ? ste.paragraphStart.toString() : null
-            source = ste.getSrcText().replaceAll("\n", /zzzspacezzz/)
-            if (source ==~ /^(<\/?[a-z]+[0-9]* ?\/?>)+$/){
+if (all_project || cli) {
+    files = project.projectFiles
+} else {
+    files = project.projectFiles.subList(editor.@displayedFileIndex, editor.@displayedFileIndex + 1)
+}
+for (i in 0 ..< files.size()) {
+    fi = files[i]
+    for (j in 0 ..< fi.entries.size()) {
+        def cellbg
+        def altbg
+        def border = "border:1px solid transparent"
+        def ignore = false
+        ste = fi.entries[j]
+        info = project.getTranslationInfo(ste)
+        def isDup = ste.getDuplicate()
+        def isAlt = info.defaultTranslation ? defStr : altStr
+        def newPar = ste.paragraphStart ? ste.paragraphStart.toString() : null
+        source = ste.getSrcText().replaceAll("\n", /zzzspacezzz/)
+        if (source ==~ /^(<\/?[a-z]+[0-9]* ?\/?>)+$/) {
             ignore = true
-            }
-            seg_num = ste.entryNum()
-            target = project.getTranslationInfo(ste) ? project.getTranslationInfo(ste).translation : null;
-            if (target == null){
-                target = "zzznullzzz"
-                if (skipUntran){
+        }
+        seg_num = ste.entryNum()
+        target = project.getTranslationInfo(ste) ? project.getTranslationInfo(ste).translation : null;
+        if (target == null){
+        target = "zzznullzzz"
+            if (skipUntran) {
                 ignore = true
-                }
             }
-            if (target.size() == 0){
-                if (fillEmptTran) 
+        }
+        if (target.size() == 0) {
+            if (fillEmptTran) {
                 target = EmptTranTxt
-                else
+            } else {
                 target = ""
             }
-            target = target.replaceAll("\n", /zzzspacezzz/)
-            source = utils.makeValidXML(source).replaceAll(/zzzspacezzz/, /<br\/>/)
-            source = painttag(source, curtagbg, curtagfg)
-            target = utils.makeValidXML(target).replaceAll(/zzznullzzz/, /&#8288;/).replaceAll(/zzzspacezzz/, /<br\/>/)
-            target = painttag(target, curtagbg, curtagfg)
-            if (isDup.toString() != 'NONE' && markNonUniq ){
-                source = (isDup.toString() == 'FIRST') ? paintseg(source, normfg) : paintseg(source, nonuniqfg)
-                target = (isDup.toString() == 'FIRST') ? paintseg(target, normfg) : paintseg(target, nonuniqfg)
-                cellbg = "bgcolor=$nonuniqbg"
-            }else{
-            	source = paintseg(source, normfg)
-            	target = paintseg(target, normfg)
-            }
-            if (isAlt == altStr && markAlt){
-                altbg = "bgcolor=$alttxtbg"
-            }
-            if (newPar){
+        }
+        target = target.replaceAll("\n", /zzzspacezzz/)
+        source = utils.makeValidXML(source).replaceAll(/zzzspacezzz/, /<br\/>/)
+        source = painttag(source, curtagbg, curtagfg)
+        target = utils.makeValidXML(target).replaceAll(/zzznullzzz/, /&#8288;/).replaceAll(/zzzspacezzz/, /<br\/>/)
+        target = painttag(target, curtagbg, curtagfg)
+        if (isDup.toString() != 'NONE' && markNonUniq) {
+            source = (isDup.toString() == 'FIRST') ? paintseg(source, normfg) : paintseg(source, nonuniqfg)
+            target = (isDup.toString() == 'FIRST') ? paintseg(target, normfg) : paintseg(target, nonuniqfg)
+            cellbg = "bgcolor=$nonuniqbg"
+        } else {
+            source = paintseg(source, normfg)
+            target = paintseg(target, normfg)
+        }
+        if (isAlt == altStr && markAlt) {
+            altbg = "bgcolor=$alttxtbg"
+        }
+        if (newPar) {
             	border = "border:1px solid transparent; border-top:5px solid  #aab7b8" 
-            	
-            }
-            if (! ignore){
-                isDup = isDup.toString().toString().replaceAll(/NONE/, uniqStr).replaceAll(/FIRST/, firstStr).replaceAll(/NEXT/, repStr)
-                extraCont = "$isDup $isAlt"
-                extraCell  = addExtraCol ? """<td style=\"$border\" $altbg width=\"$extraWidth%\">$extraCont</td>""" : ""
-                table_contents << """
+        }
+        if (! ignore) {
+            isDup = isDup.toString().toString().replaceAll(/NONE/, uniqStr).replaceAll(/FIRST/, firstStr).replaceAll(/NEXT/, repStr)
+            extraCont = "$isDup $isAlt"
+            extraCell  = addExtraCol ? """<td style=\"$border\" $altbg width=\"$extraWidth%\">$extraCont</td>""" : ""
+            table_contents << """
   <tr>
     $extraCell
     <td $cellbg style=\"$border\" width=\"$srcWidth%\">$source</td>
     <td $cellbg style=\"$border\" width=\"$targWidth%\">$target</td>
   </tr>"""
-                count++
-            }
+            count++
         }
-        table_contents << "  </table>\n"
     }
+    table_contents << "  </table>\n"
+}
+
 table_contents << "</body>\n</html>"
 
-if (count > 0){
+if (count > 0) {
     table.write(table_contents.toString().replaceAll("\\n\\s+\\n", '\n').trim(), 'UTF-8')
-    console.println(utils.format(resBundle("countWritten", countWritten), count, table))
+    echo(utils.format(resBundle("countWritten", countWritten), count, table))
     mainWindow.statusLabel.setText(utils.format(resBundle("tableWritten", tableWritten), table, count))
     Timer timer = new Timer().schedule({mainWindow.statusLabel.setText(null)} as TimerTask, 10000)
 
-    if (autoopen in ["folder", "table"]){
+    if (autoopen in ["folder", "table"]) {
         autoopen = evaluate("${autoopen}")
-        console.println autoopen
+        echo("Opening " + autoopen)
         Desktop.getDesktop().open(new File(autoopen.toString()))
     }
-
 } else {
-    console.println(utils.format(resBundle("noTable", noTable), table))
-    mainWindow.statusLabel.setText(utils.format(resBundle("noTable", noTable), table))
-    Timer timer = new Timer().schedule({mainWindow.statusLabel.setText(null)} as TimerTask, 5000)
+    echo(utils.format(resBundle("noTable", noTable), table))
+    if (!cli) {
+        mainWindow.statusLabel.setText(utils.format(resBundle("noTable", noTable), table))
+        Timer timer = new Timer().schedule({mainWindow.statusLabel.setText(null)} as TimerTask, 5000)
+    }
 }
-
 return

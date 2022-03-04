69c5d583ed27115f48c1a103e10def953517c8f8
Kos Ivantsov <kos.ivantsov@gmail.com>
2022-03-04 12:30:56 +0200

    Add files via upload

---
+++ aux_scripts/goto_next_nonunique.groovy
@@ -0,0 +1,44 @@
+/* :name=GoTo - Next non-unique segment :description=Jump to next non-unique
+ *  
+ *  @author:   Kos Ivantsov
+ *  @date:     2021-05-01
+ *  @version:  0.1         
+ */
+ 
+import static javax.swing.JOptionPane.*
+import static org.omegat.util.Platform.*
+
+prop = project.projectProperties
+exit = false
+if (!prop) {
+  final def title = 'Next non-unique'
+  final def msg   = 'Please try again after you open a project.'
+  showMessageDialog null, msg, title, INFORMATION_MESSAGE
+  exit = true
+  return
+}
+
+lastSegmentNumber = project.allEntries.size()
+jump = false
+def gui() {
+    if (exit)
+    return
+    ste = editor.getCurrentEntry()
+    currentSegmentNumber = ste.entryNum()
+    //jump = false
+    while (!jump) {
+        nextSegmentNumber = currentSegmentNumber == lastSegmentNumber ? 1 : currentSegmentNumber + 1
+        stn = project.allEntries[nextSegmentNumber -1]
+        info = project.getTranslationInfo(stn)
+        isDup = stn.getDuplicate()
+        if ("$isDup" != "NONE") {
+            jump = true
+            editor.gotoEntry(nextSegmentNumber)
+            return
+        } else {
+            jump = false
+            currentSegmentNumber = nextSegmentNumber
+        }
+    }
+}
+return

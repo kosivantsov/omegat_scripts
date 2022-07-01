6758d95f301b725dc11fbf96fbf371d45fa26115
Kos Ivantsov <kos.ivantsov@gmail.com>
2022-07-01 13:37:37 +0200

    goto next xauto and xenforce scripts added

---
+++ aux_scripts/goto_next_xauto.groovy
@@ -0,0 +1,47 @@
+/* :name=GoTo - Next x-auto :description=Jump to next segment with x-auto translation
+ *  
+ *  @author:   Kos Ivantsov
+ *  @date:     2021-07-01
+ *  @version:  0.1         
+ */
+ 
+import static javax.swing.JOptionPane.*
+import static org.omegat.util.Platform.*
+
+prop = project.projectProperties
+exit = false
+if (!prop) {
+  final def title = 'Next x-auto'
+  final def msg   = 'Please try again after you open a project.'
+  showMessageDialog null, msg, title, INFORMATION_MESSAGE
+  exit = true
+  return
+}
+
+lastSegmentNumber = project.allEntries.size()
+jump = false
+def gui() {
+    if (exit)
+    return
+    ste = editor.getCurrentEntry()
+    currentSegmentNumber = startingSegmentNumber = ste.entryNum()
+    //jump = false
+    while (!jump) {
+        nextSegmentNumber = currentSegmentNumber == lastSegmentNumber ? 1 : currentSegmentNumber + 1
+        stn = project.allEntries[nextSegmentNumber -1]
+        info = project.getTranslationInfo(stn)
+        isXAuto = info.linked.toString()
+        if (isXAuto == "xAUTO") {
+            jump = true
+            editor.gotoEntry(nextSegmentNumber)
+            return
+        } else {
+            jump = false
+            currentSegmentNumber = nextSegmentNumber
+        }
+        if (nextSegmentNumber == startingSegmentNumber) {
+            return
+        }
+    }
+}
+return
---
+++ aux_scripts/goto_next_xenforce.groovy
@@ -0,0 +1,48 @@
+/* :name=GoTo - Next x-enforce :description=Jump to next segment with x-enforce translation
+ *  
+ *  @author:   Kos Ivantsov
+ *  @date:     2021-07-01
+ *  @version:  0.1         
+ */
+ 
+import static javax.swing.JOptionPane.*
+import static org.omegat.util.Platform.*
+
+prop = project.projectProperties
+exit = false
+if (!prop) {
+  final def title = 'Next x-enforce'
+  final def msg   = 'Please try again after you open a project.'
+  showMessageDialog null, msg, title, INFORMATION_MESSAGE
+  exit = true
+  return
+}
+
+lastSegmentNumber = project.allEntries.size()
+jump = false
+def gui() {
+    if (exit)
+    return
+    ste = editor.getCurrentEntry()    
+    currentSegmentNumber = startingSegmentNumber = ste.entryNum()
+    def nextSegmentNumber
+    //jump = false
+    while (!jump) {
+        nextSegmentNumber = currentSegmentNumber == lastSegmentNumber ? 1 : currentSegmentNumber + 1
+        stn = project.allEntries[nextSegmentNumber -1]
+        info = project.getTranslationInfo(stn)
+        isXEnforce = info.linked.toString()
+        if (isXEnforce == "xENFORCED") {
+            jump = true
+            editor.gotoEntry(nextSegmentNumber)
+            return
+        } else {
+            jump = false
+            currentSegmentNumber = nextSegmentNumber
+        }
+        if (nextSegmentNumber == startingSegmentNumber) {
+            return
+        }
+    }
+}
+return

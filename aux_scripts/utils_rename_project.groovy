112c52c1f10373f573232e972f54c1a4523172a3
Kos Ivantsov <kos.ivantsov@gmail.com>
2024-03-26 12:01:54 +0100

    Update utils_rename_project.groovy

--- aux_scripts/utils_rename_project.groovy
+++ aux_scripts/utils_rename_project.groovy
@@ -2,7 +2,8 @@
  * 
  * @author  Kos Ivantsov
  * @date    2024-03-25
- * @version 0.0.1
+ * @latest  2024-03-26
+ * @version 0.0.2
  * 
  */
 
@@ -18,14 +19,18 @@ configDir = StaticUtils.getConfigDir()
 omtDir = prop.getProjectRoot() + "omegat"
 projectDir = prop.getProjectRoot()
 projectName = prop.projectName
+omtEnding = projectName.find(/[_\.]?[oO][mM][tT]$/) ? projectName.find(/[_\.]?[oO][mM][tT]$/) : ""
 parentDir =  (projectDir - projectName).replaceAll(/[\\\\\/]+$/, "") 
 projectNameFile = new File(omtDir + File.separator + "project_name.txt")
 timestamp = new Date().format("yyyyMMddHHmmss")
 if (projectNameFile.exists()) {
     newProjectName = projectNameFile.text ? projectNameFile.readLines().get(0) : projectName
+    omtEnding = newProjectName.find(/[_\.]?[oO][mM][tT]$/) ? "" : omtEnding
+    newProjectName = newProjectName + omtEnding
     newProjectDir = new File(parentDir + File.separator + newProjectName)
+    console.println newProjectDir
     if (newProjectName != projectName) {
-        rename =  true
+        rename = true
         if (newProjectDir.exists()) {
             backupExisting = new File(newProjectDir.toString() + "." + timestamp)
             rename = newProjectDir.renameTo(backupExisting)
@@ -35,6 +40,8 @@ if (projectNameFile.exists()) {
             sleep 3000
             new File(projectDir).renameTo(newProjectDir)
             ProjectUICommands.projectOpen(newProjectDir, true)
+        } else {
+            console.println("✘ Failed to rename the project")
         }
     }
 }

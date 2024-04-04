350aade5a07d1c9f15315896a52bd09fc06e6a6b
Kos Ivantsov <kos.ivantsov@gmail.com>
2024-04-04 20:29:20 +0200

    Update utils_custom_tags.groovy

--- aux_scripts/utils_custom_tags.groovy
+++ aux_scripts/utils_custom_tags.groovy
@@ -2,23 +2,34 @@
  * 
  * @author:  Kos Ivantsov
  * @date:    2024-03-26
- * @latest:  2024-03-28
- * @version: 1.0
+ * @latest:  2024-04-04
+ * @version: 1.2
  * 
  */
 
+import org.omegat.gui.main.ProjectUICommands
 import org.omegat.util.Preferences
 import org.omegat.util.StaticUtils
 import static org.omegat.core.events.IProjectEventListener.PROJECT_CHANGE_TYPE.*
 
+omtPid = Long.parseLong(java.lang.management.ManagementFactory.getRuntimeMXBean().getName().split("@")[0])
+currentTags = Preferences.getPreference("tagValidation_customPattern")
+currentFlaggedText = Preferences.getPreference("tagValidation_removePattern")
+if (System.Properties.getProperty("${omtPid}_appliedTagFile")) {
+    appliedTagFile = new File(System.Properties.getProperty("${omtPid}_appliedTagFile"))
+    appliedTagFile.write(currentTags, "UTF-8")
+}
+if (System.Properties.getProperty("${omtPid}_appliedFlaggedFileProperty")) {
+    appliedFlaggedFile = new File(System.Properties.getProperty("${omtPid}_appliedFlaggedFileProperty"))
+    appliedFlaggedFile.write(currentFlaggedText, "UTF-8")
+}
 
 prop = project.projectProperties
 if (! prop) {
-    console.println("No project open")
+//    console.println("No project open")
     return
 }
-currentTags = Preferences.getPreference("tagValidation_customPattern")
-currentFlaggedText = Preferences.getPreference("tagValidation_removePattern")
+
 configDir = StaticUtils.getConfigDir()
 omtDir = prop.getProjectRoot() + "omegat" + File.separator
 tagFileName = "omegat.customtags"
@@ -27,6 +38,7 @@ globalTagFile = new File(configDir + tagFileName)
 globalFlaggedFile = new File(configDir + flaggedFileName)
 projectTagFile = new File(omtDir + tagFileName)
 projectFlaggedFile = new File(omtDir + flaggedFileName)
+reload = false
 switch (eventType) {
     case LOAD:
         if (! globalTagFile.exists()) {
@@ -37,19 +49,23 @@ switch (eventType) {
         }
         appliedTagFile = projectTagFile.exists() ? projectTagFile : globalTagFile
         appliedFlaggedFile = projectFlaggedFile.exists() ? projectFlaggedFile : globalFlaggedFile
-        System.Properties.setProperty("applideTagProperty", appliedTagFile.toString())
-        System.Properties.setProperty("applideFlaggedProperty", appliedFlaggedFile.toString())
+        System.Properties.setProperty("${omtPid}_appliedTagFile", appliedTagFile.toString())
+        System.Properties.setProperty("${omtPid}_appliedFlaggedFile", appliedFlaggedFile.toString())
+        System.Properties.setProperty("${omtPid}_appliedTagValue", currentTags)
+        System.Properties.setProperty("${omtPid}_appliedFlaggedValue", currentFlaggedText)
         Preferences.setPreference("tagValidation_customPattern", appliedTagFile.text)
         Preferences.setPreference("tagValidation_removePattern", appliedFlaggedFile.text)
         Preferences.save()
-
-    case CLOSE:
-        appliedTagFile = new File(System.Properties.getProperty("applideTagProperty"))
-        appliedFlaggedFile = new File(System.Properties.getProperty("applideFlaggedProperty"))
-        if (appliedTagFile.text != currentTags) {
-            appliedTagFile.write(currentTags, "UTF-8")
+        currentSavedTags = Preferences.getPreference("tagValidation_customPattern")
+        currentSavedFlaggedText = Preferences.getPreference("tagValidation_removePattern")
+        if (currentSavedTags != System.Properties.getProperty("${omtPid}_appliedTagValue")) {
+            reload = true
         }
-        if (appliedFlaggedFile.text != currentFlaggedText) {
-            appliedFlaggedFile.write(currentFlaggedText, "UTF-8")
+        if (currentSavedFlaggedText != System.Properties.getProperty("${omtPid}_appliedFlaggedValue")) {
+            reload = true
+        }
+        if (reload) {
+            sleep 500
+            ProjectUICommands.projectReload()
         }
 }
